32098dc9c128443c2acaf962dd76d656c9548666
Callum White <calw24@icloud.com>
2021-10-15 14:37:58 +0100

    csv file into 2d array

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Threading;
+
+
+class Program {
+
+    //static variables
+    static string path = "hexColours.csv";
+
+  public static void Main (string[] args) {
+    readInHexColours();
+  }
+
+  static void readInHexColours()
+  {
+
+
+
+
+
+      string[] hexColours = new string[File.ReadAllLines(path).Length];
+      int colums = 2;
+      int rows = hexColours.Length;
+      string [,] colours = new string[rows, colums];
+      
+      try
+      {
+        hexColours = File.ReadAllLines(path);
+      }
+
+      catch(Exception e)
+      {
+        Console.WriteLine("oopsie daisies " + e.Message);
+      }
+
+      string[] temp = new string[colums];
+
+      for(int i = 0; i < rows;i++)
+      {
+        temp = hexColours[i].Split(',');
+        for(int j = 0; j< colums;j++)
+        {
+          colours[i,j] = temp[j];
+        }
+      }
+
+
+
+      for(int l = 0; l < colours.GetLength(0);l++)
+      {
+        for(int h = 0; h < colours.GetLength(1);h++)
+        {
+          Console.Write(colours[l,h] + ",");
+        }
+        Console.WriteLine();
+      }
+
+
+      
+  }
+  
+  
+}
